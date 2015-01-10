700f2b3b91aece1206fb6d1329651f5d9e50af93
Michael Shirts <michael.shirts@virginia.edu>
2015-01-09 21:20:29 -0500

    Update of parallel tempering to work with new mbar.py
    * Not clear the README actually applies to this file.  It will need a
    readthrough to figure out what belongs and what doesn't.
    
    * Removed histogram_wham code and reading saved free energies, since it
      doesn't appear necessary anymore
    
    * switched to computePMF

--- parallel-tempering-2dpmf/parallel-tempering-2dpmf.py
+++ parallel-tempering-2dpmf/parallel-tempering-2dpmf.py
@@ -5,7 +5,6 @@
 # PROTOCOL
 #
 # * Potential energies and (phi, psi) torsions from parallel tempering simulation are read in by temperature
-# * WHAM [2] is used to rapidly generate an initial guess for the dimensionless free energies f_k.
 # * Replica trajectories of potential energies and torsions are reconstructed to reflect their true temporal
 # correlation, and then subsampled to produce statistically independent samples, collecting them again by temperature
 # * The MBAR class is initialized with this initial guess at dimensionless free energies f_k, reducing time for
@@ -21,9 +20,6 @@
 # J. Chem. Phys. 129:124105, 2008
 # http://dx.doi.org/10.1063/1.2978177
 #
-# [2] Kumar S, Bouzida D, Swensen RH, Kollman PA, and Rosenberg JM. The weighted histogram analysis method
-# for free-energy calculations on biomolecules. I. The Method. J. Comput Chem. 13:1011, 1992.
-
 #===================================================================================================
 # IMPORTS
 #===================================================================================================
@@ -48,6 +44,7 @@ kB = 1.3806503 * 6.0221415 / 4184.0 # Boltzmann constant in kcal/mol/K
 
 data_directory = 'data/' # directory containing the parallel tempering data
 temperature_list_filename = os.path.join(data_directory, 'temperatures') # file containing temperatures in K
+free_energies_filename = 'f_k.out'
 potential_energies_filename = os.path.join(data_directory, 'energies', 'potential-energies') # file containing total energies (in kcal/mol) for each temperature and snapshot
 trajectory_segment_length = 20 # number of snapshots in each contiguous trajectory segment
 niterations = 500 # number of iterations to use
@@ -97,182 +94,6 @@ def logSum(log_terms):
 
    # return the log sum
    return log_sum
-
-def histogram_wham(beta_k, U_kn, N_k, nbins = 100, bin_width = None, maximum_iterations = 5000, relative_tolerance = 1.0e-8, initial_f_k = None):
-   """Construct an initial guess of the f_k by histogram reweighting (specifically, WHAM [2]).
-
-   ARGUMENTS
-     beta_k (numpy K array) - inverse temperatures (in units of 1/energy)
-     U_kn (numpy K x N_max array) - potential energies (in units of energy)
-     N_k (numpy K array of numpy.int32) - number of samples per states, N_max = N_k.max()
-
-   OPTIONAL ARGUMENTS
-     nbins (int) - if specified, sets the number of bins to use (default: 100)
-     bin_width (float) - if specified, sets the bin width (overrides nbins) (defulat: None)
-     maximum_iterations (int) - maximum number of iterations to use
-     relative_tolerance (floeat) - relative convergence tolerance (default: 1.0e-8)
-
-   RETURNS
-     f_k (numpy K array) - guess at initial state dimensionless free energies
-
-   REFERENCE
-     [2] Kumar S, Bouzida D, Swensen RH, Kollman PA, and Rosenberg JM. The weighted histogram analysis method
-     for free-energy calculations on biomolecules. I. The Method. J. Comput Chem. 13:1011, 1992.
-   
-   """
-
-   # Get sizes
-   K = N_k.size
-   N_max = N_k.max()
-
-   # Create a list of indices of all configurations in kn-indexing.
-   mask_kn = numpy.zeros([K,N_max], dtype=numpy.bool)
-   for k in range(0,K):
-      mask_kn[k,0:N_k[k]] = True
-   # Create a list from this mask.
-   sample_indices = numpy.where(mask_kn)
-
-   # Construct histogram bins
-   M = nbins # number of energy bins
-   SMALL = 1.0e-6
-   U_min = U_kn[sample_indices].min()
-   U_max = U_kn[sample_indices].max()
-   U_max += (U_max - U_min) * SMALL # increment by a bit
-   delta_U = (U_max - U_min) / float(M)
-   if (bin_width != None):
-      delta_U = bin_width
-      M = int(numpy.ceil((U_max - U_min) / bin_width))
-      print "Using %d bins to achieve bin width of %f" % (M, delta_U)
-   else:
-      print "Bin width is %f energy units to achieve nbins = %d" % (delta_U, M)
-   U_m = U_min + delta_U * (0.5 + numpy.arange(0,M,dtype = numpy.float64))
-   H_m = numpy.zeros([M], numpy.float64)
-   # assign snapshots to energy bins
-   bin_kn = numpy.zeros([K,N_max], numpy.int32) # bin_kn[k,n] is bin index of U_kn[k,n]
-   bin_kn[sample_indices] = numpy.array(((U_kn[sample_indices] - U_min) / delta_U), numpy.int32)
-   H_m = numpy.bincount(bin_kn[sample_indices])
-   
-   # compute logs of various quantities
-   LOG_ZERO = -1000.0 # replacement for log(0)
-   log_H_m = numpy.ones([M], numpy.float64) * LOG_ZERO
-   for m in range(M):
-      if (H_m[m] > 0):
-         log_H_m[m] = log(H_m[m])
-   log_N_k = numpy.ones([K], numpy.float64) * LOG_ZERO
-   for k in range(K):
-      if (N_k[k] > 0):
-         log_N_k[k] = log(N_k[k])
-
-   # initialize free energies
-   f_k = numpy.zeros([K], numpy.float64)
-   if (initial_f_k != None):
-      f_k = initial_f_k.copy()
-
-   # iterate
-   f_k_new = numpy.zeros([K], numpy.float64)
-   max_delta = 0.0
-   for iteration in range(maximum_iterations):
-      # print "iteration %d" % iteration
-      
-      # Form auxiliary matrices, used in both self-consistent iteration and Newtom-Raphson.
-      # log space
-      log_w_mi = numpy.zeros([M,K], numpy.float64)
-      for m in range(M):
-         # denominator = \sum_k N_k exp[f_k - \beta_k U_m]
-         exp_arg_k = log_N_k[:] + f_k[:] - beta_k[:]*U_m[m]
-         log_w_mi[m,:] = exp_arg_k[:] - logSum(exp_arg_k[:])
-      # real space
-      w_mi = numpy.zeros([M,K], numpy.float64)
-      for m in range(M):
-         exp_arg_k = f_k[:] - beta_k[:]*U_m[m]
-         max_arg = exp_arg_k.max()
-         numerators_i = N_k[:] * numpy.exp(exp_arg_k[:] - max_arg)
-         w_mi[m,:] = numerators_i[:] / numerators_i.sum()
-         
-      # Compute new estimates of log weights using self-consistent iteration.
-      for i in range(K):
-         # Compute new estimate of log weights.
-         f_k_new[i] = f_k[i] + log(N_k[i]) - logSum(log_H_m[:] + log_w_mi[:,i])
-      
-      # shift to ensure f_k_new[0] = 0.0
-      f_k_new -= f_k_new[0]
-      # store difference
-      Delta_f_k = f_k_new - f_k
-      # update f_k
-      f_k = f_k_new.copy()
-
-      # If all f_k are zero, terminate
-      if numpy.all(f_k == 0.0):
-        break
-      
-      # Terminate when max((f - fold) / f) < relative_tolerance for all nonzero f.
-      max_delta = (numpy.abs(Delta_f_k) / (numpy.abs(f_k_new)).max()).max()
-      print "iteration %8d relative max_delta = %8e" % (iteration, max_delta)
-      if numpy.isnan(max_delta) or (max_delta < relative_tolerance):
-         break
-
-   # if unconverged
-   if (iteration == maximum_iterations):
-      print "Did not converge in %d iterations (final relative tolerance %e)" % (maximum_iterations, max_delta)
-      print "f_k = "
-      print f_k
-      return f_k
-   
-   # summary
-   print "Converged to relative tolerance of %e (convergence tolerance %e) in %d iterations" % (max_delta, relative_tolerance, iteration)
-   print "f_k = "
-   print f_k
-
-   # return the estimate of the dimensionless free energies
-   return f_k
-
-def write_free_energies(filename, f_k):
-   """Write free energies to the given file.
-
-   ARGUMENTS
-     filename (string) - the name of the file to write to
-     f_k (numpy array) - weights to write
-   """
-
-   # get size
-   K = f_k.size
-
-   # Write to file.
-   outfile = open(filename, 'w')
-   for k in range(K):
-      outfile.write("%f " % f_k[k])
-   outfile.write("\n")
-   outfile.close()
-
-   return
-
-def read_free_energies(filename):
-   """Read free energies from the given file.
-
-   ARGUMENTS
-     filename (string) - the name of the file to write to
-   """
-
-   print "Reading free energies from %s..." % filename
-
-   # read file
-   infile = open(filename, 'r')
-   lines = infile.readlines()
-   infile.close()
-
-   # split into elements
-   elements = list()
-   for line in lines:
-      elements += line.split()
-   print elements
-
-   # parse
-   K = len(elements)
-   f_k = numpy.zeros([K], numpy.float64)
-   for k in range(K):
-      f_k[k] = float(elements[k])
-
-   return f_k
 
 #===================================================================================================
 # MAIN
@@ -474,30 +295,14 @@ for i in range(nbins):
    print "bin %5d (%6.1f, %6.1f) %12d conformations" % (i, bin_centers[i][0], bin_centers[i][1], bin_counts[i])
 
 #===================================================================================================
-# Read free energies, if they exist.
-# Otherwise, use WHAM to quickly compute an initial guess of dimensionless free energies f_k.
-# Then initialize MBAR.
+# Initialize MBAR.
 #===================================================================================================
 
-free_energies_filename = 'f_k.out'
-if os.path.exists(free_energies_filename):
-   # Read free energies
-   print "Reading free energies from %s ..." % free_energies_filename
-   f_k = read_free_energies(free_energies_filename)
+# Compute initial guess using histogram WHAM.
+print "Using WHAM to generate histogram-based initial guess of dimensionless free energies f_k..."
 
-   # Initialize MBAR without any iterations.
-   mbar = pymbar.MBAR(u_kln, N_k, maximum_iterations = 0, verbose = True, initial_f_k = f_k)
-else:
-   # Compute initial guess using histogram WHAM.
-   print "Using WHAM to generate histogram-based initial guess of dimensionless free energies f_k..."
-   f_k = histogram_wham(beta_k, U_kn, N_k)
-
-   # Initialize MBAR with Newton-Raphson
-   print "Initializing MBAR (will estimate free energy differences first time)..."
-   mbar = pymbar.MBAR(u_kln, N_k, verbose = True, initial_f_k = f_k)
-
-   # Write free energies
-   write_free_energies(free_energies_filename, mbar.f_k)
+# Initialize MBAR
+mbar = pymbar.MBAR(u_kln, N_k, verbose = True)
 
 #===================================================================================================
 # Compute PMF at the desired temperature.
@@ -510,16 +315,15 @@ target_beta = 1.0 / (kB * target_temperature)
 u_kn = target_beta * U_kn
 # Compute PMF at this temperature, returning dimensionless free energies and uncertainties.
 # f_i[i] is the dimensionless free energy of bin i (in kT) at the temperature of interest
-# d2f_ij[i,j] is an estimate of the covariance in the estimate of (f_i[i] - f_j[j])
-(f_i, d2f_ij) = mbar.computePMF_states(u_kn, bin_kn, nbins)
-
-# Find index of bin with lowest free energy.
-imin = f_i.argmin()
+# df_i[i,j] is an estimate of the covariance in the estimate of (f_i[i] - f_j[j], with reference 
+# the lowest free energy state.
+(f_i, df_i) = mbar.computePMF(u_kn, bin_kn, nbins, uncertainties='from-lowest')
 
 # Show free energy and uncertainty of each occupied bin relative to lowest free energy
 print "2D PMF"
 print ""
 print "%8s %6s %6s %8s %10s %10s" % ('bin', 'phi', 'psi', 'N', 'f', 'df')
+
 for i in range(nbins):
-   print '%8d %6.1f %6.1f %8d %10.3f %10.3f' % (i, bin_centers[i][0], bin_centers[i][1], bin_counts[i], f_i[i], sqrt(d2f_ij[i,imin]))
+   print '%8d %6.1f %6.1f %8d %10.3f %10.3f' % (i, bin_centers[i][0], bin_centers[i][1], bin_counts[i], f_i[i], df_i[i])
 
